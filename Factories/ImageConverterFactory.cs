104873aaa654dc181b7a7800aa11a1ef2f70c191
LeylinFeyler <vladuslav0811@gmail.com>
2025-05-30 16:04:01 +0300

    update image factory

--- Factories/ImageConverterFactory.cs
+++ Factories/ImageConverterFactory.cs
@@ -11,11 +11,14 @@ namespace UltimateConverter.Factories
             {
                 "png" => new PngConverter(),
                 "jpg" => new JpgConverter(),
+                "jpeg" => new JpgConverter(),
                 "gif" => new GifConverter(),
                 "bmp" => new BmpConverter(),
+                "tif" => new TiffConverter(),
                 "tiff" => new TiffConverter(),
                 "webp" => new WebpConverter(),
                 "heic" => new HeicConverter(),
+                "heif" => new HeicConverter(),
                 _ => throw new NotSupportedException($"Image format '{format}' is not supported.")
             };
         }
