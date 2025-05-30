2ad7053ba55a6c4d9cae8364605016227d15e655
LeylinFeyler <vladuslav0811@gmail.com>
2025-05-30 16:05:18 +0300

    add video converters

---
+++ Converters/VideoConverter/MkvConverter.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using UltimateConverter.Interfaces;
+using UltimateConverter.Services;
+
+namespace UltimateConverter.Converters.VideoConverter
+{
+    internal class MkvConverter : IConverter
+    {
+        public void Convert(string inputFilePath)
+        {
+            string[] files = inputFilePath.Split(' ');
+            string videoFile = string.Empty;
+            foreach (string file in files)
+            {
+                videoFile = file.Trim(Path.GetExtension(file).ToCharArray());
+                ExternalToolRunner.RunCommand("HandBrakeCLI", "--input " + file + " --output " + videoFile + ".mkv");
+            }
+            Console.WriteLine($"Converted successfully!");
+        }
+    }
+}
