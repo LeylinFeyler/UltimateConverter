8fb15b6426d5eb5c704811ef396141fe0bf5b4bc
LeylinFeyler <vladuslav0811@gmail.com>
2025-05-30 16:12:35 +0300

    update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using UltimateConverter.Interfaces;
+using UltimateConverter.Factories;
+
+namespace UltimateConverter
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Usage: UltimateConverter <-i|-v|-a|-f> <outputFormat> <inputFilePath>");
+                return;
+            }
+
+            string typeArg = args[0];
+            string outputFormat = args[1];
+            string inputFilePath = "";
+            foreach (string arg in args[2..])
+                inputFilePath += "\"" + arg + "\" ";
+
+            IConverterFactory factory;
+            switch (typeArg)
+            {
+                case "-i":
+                    factory = new ImageConverterFactory();
+                    break;
+                case "-v":
+                    factory = new VideoConverterFactory();
+                    break;
+                //case "-a":
+                //    factory = new AudioConverterFactory();
+                //    break;
+                //case "-f":
+                //    factory = new FileConverterFactory();
+                //    break;
+                default:
+                    Console.WriteLine("Unknown conversion type. Use -i (image), -v (video), -a (audio), -f (file).");
+                    return;
+            }
+
+            IConverter converter = factory.CreateConverter(outputFormat);
+            if (converter == null)
+            {
+                Console.WriteLine($"Unsupported output format: {outputFormat}");
+                return;
+            }
+
+            converter.Convert(inputFilePath);
+        }
+    }
+}
